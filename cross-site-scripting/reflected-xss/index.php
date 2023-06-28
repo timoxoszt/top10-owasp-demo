5c5a01714e081564ce0b3189c77a2090e3a03589
timoxoszt <20520810@gm.uit.edu.vn>
2023-06-28 19:53:43 +0700

    reflected xss

---
+++ cross-site-scripting/reflected-xss/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gug0</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <center>
+        <h1>Gug0</h1>
+    </center>
+    
+    <form class="mb-3" action="search.php" method="GET">
+        <div class="input-group">
+            <input type="text" class="form-control" name="q" placeholder="Search here ..." required>
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+    </form>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ cross-site-scripting/reflected-xss/search.php
@@ -0,0 +1,6 @@
+<?php
+    $search = $_GET['q'];
+    echo "<h1>Search result for: " . $search . "</h1>";
+    echo "<hr>";
+    echo "<p>Sorry, we don't have any result for: " . $search . "</p>";
+?>
